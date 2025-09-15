b0ba8574f63667fce67f4b7ee0764bf66cdf7826
muhammed-anees-pp <muhammedanees.work@gmail.com>
2025-09-15 15:34:07 +0530

    chore: set up basic file structure for components and pages

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="navbar-left">
+
+        </div>
+        <div className="navbar-right">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
